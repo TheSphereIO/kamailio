ecf976adb3844981901a39dc177da3e952f83b6d
Federico Cabiddu <federico.cabiddu@gmail.com>
2017-04-18 16:57:42 +0000

    use TheSphereIO Kamailio's fork to build packages

--- build-kamailio.sh
+++ build-kamailio.sh
@@ -1,28 +1,47 @@
 #!/bin/bash
 
-GITREF="master"
-VERSION="5.0.0"
-
 mkdir -p rpmbuild/SOURCES
 mkdir -p rpmbuild/RPMS
 mkdir -p rpmbuild/SPECS
 
 # clone Kamailio repository
 
-git clone https://github.com/kamailio/kamailio.git
+#git clone https://github.com/kamailio/kamailio.git
+git clone https://github.com/TheSphereIO/kamailio-src.git
+mv kamailio-src kamailio
 
 cd kamailio
 # switch to the requested gitref
+if [ -z "$1" ]
+then
+    GITREF="master"
+else
+    GITREF=$1
+fi
 
 git checkout ${GITREF}
+
+LASTCOMMIT=$(git rev-parse --short HEAD)                                                                                                                                                     
+TAG=`git tag --points-at ${LASTCOMMIT}`                                                                                                                                                      
+                                                                                                                                                                                             
+if [ ! -z "$TAG" ]                                                                                                                                                                           
+then                                                                                                                                                                                         
+    VERSION=${TAG}                                                                                                                                                                       
+else                                                                                                                                                                                         
+    VERSION=`git describe --always`                                                                                                                                                      
+    VERSION=${VERSION//-/.}                                                                                                                                                                  
+fi
 
 echo "Building Kamailio $VERSION packages"
 
 git archive --output ../rpmbuild/SOURCES/kamailio-${VERSION}_src.tar.gz --prefix=kamailio-${VERSION}/  $GITREF
 cd ..
 
-cp kamailio.spec rpmbuild/SPECS/
+#cp kamailio/pkg/kamailio/centos/7/kamailio.spec rpmbuild/SPECS/
+cp kamailio/pkg/kamailio/centos/7/kamailio.spec .
+sed -i "s/%define ver.*/%define ver ${VERSION}/g" kamailio.spec
+sed -i "s/%define rel.*//g" kamailio.spec
 
 # now build the rpms
-cd rpmbuild
-rpmbuild  --define "_topdir `pwd`" -bb ./SPECS/kamailio.spec
+rpmbuild  --define "_topdir `pwd`/rpmbuild" -bb kamailio.spec
+rm -rf kamailio
